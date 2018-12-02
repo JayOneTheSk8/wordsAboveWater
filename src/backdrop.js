7baa3f0227fffd06059ca2eb3cf9ef400fc15015
Justin <misterjustincox@gmail.com>
2018-12-01 21:50:33 -0500

    Color water and sky

--- src/backdrop.js
+++ src/backdrop.js
@@ -1,30 +1,23 @@
 const canvas = document.getElementById('backdrop');
 const context = canvas.getContext('2d');
 
-
 let waterPosX = 0;
-let waterPosY = 140;
-
-function drawVowel() {
-  context.beginPath();
-  context.fillRect(20, 5, 10, 10);
-  context.fillStyle = 'black';
-  context.stroke();
-}
+let waterPosY = 900;
 
 function drawWater() {
   context.beginPath();
-  context.fillRect(waterPosX, waterPosY, canvas.width, canvas.height);
-  context.fillStyle = 'skyblue';
+  context.fillStyle = '#009BFE';
+  context.shadowColor = '#009BFE';
+  context.shadowBlur = 13;
   context.fill();
+  context.fillRect(waterPosX, waterPosY, canvas.width, canvas.height);
 }
 
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   drawWater();
-  drawVowel();
   waterPosY--;
 }
 
-draw();
-setInterval(draw, 1500);
+drawWater();
+setInterval(draw, 200);
