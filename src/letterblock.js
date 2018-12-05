3f6ad7ff7d624898554248eebfc6d04861034c22
Justin <misterjustincox@gmail.com>
2018-12-05 11:22:22 -0500

    Fix letter positioning

--- src/letterblock.js
+++ src/letterblock.js
@@ -8,6 +8,64 @@ class LetterBlock {
     this.value = this.values()[letter.toUpperCase()];
   }
 
+  draw() {
+    this.context.beginPath();
+    this.context.fillStyle = this.color;
+    this.context.lineWidth = '6';
+    this.context.shadowBlur = 0;
+    this.context.rect(this.x, this.y, 50, 50);
+    this.context.stroke();
+    this.context.fill();
+    this.context.closePath();
+    this.drawLetter();
+  }
+
+  drawLetter() {
+    this.context.beginPath();
+    this.context.font = 'bold 30px Arial';
+    this.context.fillStyle = 'white';
+    this.context.fillText(
+      this.letter,
+      this.x + this.letterPos()[this.letter],
+      this.y + 35
+    );
+  }
+
+  cover() {
+    this.color = 'grey';
+  }
+
+  letterPos() {
+    return {
+      A: 14,
+      B: 14,
+      C: 14,
+      D: 14,
+      E: 14,
+      F: 15,
+      G: 13,
+      H: 14,
+      I: 20,
+      J: 16,
+      K: 14,
+      L: 16,
+      M: 13,
+      N: 15,
+      O: 13,
+      P: 15,
+      Q: 13,
+      R: 13,
+      S: 15,
+      T: 15.5,
+      U: 14,
+      V: 15,
+      W: 11,
+      X: 14,
+      Y: 14.5,
+      Z: 15,
+    };
+  }
+
   values() {
     return {
       A: 1,
@@ -37,29 +95,6 @@ class LetterBlock {
       Y: 4,
       Z: 10,
     };
-  }
-
-  draw() {
-    this.context.beginPath();
-    this.context.fillStyle = this.color;
-    this.context.lineWidth = "6"
-    this.context.shadowBlur = 0;
-    this.context.rect(this.x, this.y, 50, 50);
-    this.context.stroke();
-    this.context.fill();
-    this.context.closePath();
-    this.drawLetter();
-  }
-
-  drawLetter() {
-    this.context.beginPath();
-    this.context.font = "bold 30px Arial";
-    this.context.fillStyle = 'white';
-    this.context.fillText(this.letter, this.x + 15, this.y + 35);
-  }
-
-  cover() {
-    this.color = 'grey';
   }
 }
 
