831e9fc0cfb205ddf1e607de9fa2f76530806d37
Justin <misterjustincox@gmail.com>
2018-12-05 11:44:00 -0500

    Create board with blocks

--- src/backdrop.js
+++ src/backdrop.js
@@ -4,21 +4,20 @@ const canvas = document.getElementById('backdrop');
 const context = canvas.getContext('2d');
 
 let a = new LetterBlock("a", 30, 20, context);
-// let c = new LetterBlock("c", 60, 20, context);
-// let d = new LetterBlock("d", 90, 20, context);
+let c = new LetterBlock("c", 130, 550, context);
+let d = new LetterBlock("d", 50, 420, context);
 let e = new LetterBlock("e", 350, 20, context);
-// let g = new LetterBlock("g", 20, 20, context);
-// let h = new LetterBlock("h", 20, 20, context);
+let g = new LetterBlock("g", 370, 570, context);
+let h = new LetterBlock("h", 370, 290, context);
 let i = new LetterBlock("i", 200, 20, context);
-// let n = new LetterBlock("n", 20, 20, context);
-// let r = new LetterBlock("r", 20, 20, context);
-// let s = new LetterBlock("s", 20, 20, context);
-// let t = new LetterBlock("t", 20, 20, context);
-// let v = new LetterBlock("v", 40, 800, context);
+let n = new LetterBlock("n", 270, 440, context);
+let r = new LetterBlock("r", 35, 280, context);
+let s = new LetterBlock("s", 120, 150, context);
+let t = new LetterBlock("t", 275, 150, context);
+let v = new LetterBlock("v", 280, 700, context);
 
 let water = new Water(context, canvas);
-const allBlocks = [a, e, i];
-// const allBlocks = [a, c, d, e, g, h, i, n, r, s, t];
+const allBlocks = [a, e, i, s, t, r, h, d, n, c, g, v];
 
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
