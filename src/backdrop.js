7b5226e3219f7ed0253288961eb6584d41acf8d9
Justin <misterjustincox@gmail.com>
2018-12-13 12:46:18 -0500

    Move index to root

--- src/backdrop.js
+++ src/backdrop.js
@@ -5,37 +5,72 @@ const GuyPhelps = require('./characters/guy_phelps');
 const Controller = require('./controller');
 const WordList = require('./wordlist');
 
-const confirmedWords = document.querySelectorAll('#wordlist');
-const canvas = document.getElementById('backdrop');
-const context = canvas.getContext('2d');
-const endAxis = 135;
+
+let confirmedWords = document.querySelectorAll('#wordlist');
+let canvas = document.getElementById('backdrop');
+let context = canvas.getContext('2d');
+let endAxis = 135;
 
 
-const wordList = new WordList;
+let wordList = new WordList;
 
-const guyPhelps = new GuyPhelps(canvas, context, 'Guy Phelps', 100, 770);
-const controller = new Controller(guyPhelps);
+let guyPhelps = new GuyPhelps(canvas, context, 'Guy Phelps', 100, 770);
+let controller = new Controller(guyPhelps);
 
-const startPlank = new Plank("left", guyPhelps, context, 0, 820, 200);
-const submitPlank = new Plank("right", guyPhelps, context, canvas.width - 80, endAxis - 1, 80);
-const leapingPlank = new Plank("right", guyPhelps, context, canvas.width - 110, endAxis + 100, 130);
+let startPlank = new Plank("left", guyPhelps, context, 0, 820, 200);
+let submitPlank = new Plank("right", guyPhelps, context, canvas.width - 80, endAxis - 1, 80);
+let leapingPlank = new Plank("right", guyPhelps, context, canvas.width - 110, endAxis + 100, 130);
 
-const a = new LetterBlock(guyPhelps, 'a', 30, 20, context);
-const c = new LetterBlock(guyPhelps, 'c', 130, 550, context);
-const d = new LetterBlock(guyPhelps, 'd', 50, 420, context);
-const e = new LetterBlock(guyPhelps, 'e', 350, 20, context);
-const g = new LetterBlock(guyPhelps, 'g', 370, 570, context);
-const h = new LetterBlock(guyPhelps, 'h', 370, 290, context);
-const i = new LetterBlock(guyPhelps, 'i', 200, 20, context);
-const n = new LetterBlock(guyPhelps, 'n', 270, 440, context);
-const r = new LetterBlock(guyPhelps, 'r', 35, 280, context);
-const s = new LetterBlock(guyPhelps, 's', 120, 150, context);
-const t = new LetterBlock(guyPhelps, 't', 275, 170, context);
-const v = new LetterBlock(guyPhelps, 'v', 280, 700, context);
+let a = new LetterBlock(guyPhelps, 'a', 30, 20, context);
+let c = new LetterBlock(guyPhelps, 'c', 130, 550, context);
+let d = new LetterBlock(guyPhelps, 'd', 50, 420, context);
+let e = new LetterBlock(guyPhelps, 'e', 350, 20, context);
+let g = new LetterBlock(guyPhelps, 'g', 370, 570, context);
+let h = new LetterBlock(guyPhelps, 'h', 370, 290, context);
+let i = new LetterBlock(guyPhelps, 'i', 200, 20, context);
+let n = new LetterBlock(guyPhelps, 'n', 270, 440, context);
+let r = new LetterBlock(guyPhelps, 'r', 35, 280, context);
+let s = new LetterBlock(guyPhelps, 's', 120, 150, context);
+let t = new LetterBlock(guyPhelps, 't', 275, 170, context);
+let v = new LetterBlock(guyPhelps, 'v', 280, 700, context);
 
-const water = new Water(context, canvas);
-const allBlocks = [a, e, i, s, t, r, h, d, n, c, g, v];
-const allPlanks = [startPlank, submitPlank, leapingPlank];
+let water = new Water(context, canvas);
+let allBlocks = [a, e, i, s, t, r, h, d, n, c, g, v];
+let allPlanks = [startPlank, submitPlank, leapingPlank];
+
+const initialise = () => {
+  confirmedWords = document.querySelectorAll('#wordlist');
+  canvas = document.getElementById('backdrop');
+  context = canvas.getContext('2d');
+  endAxis = 135;
+
+
+  wordList = new WordList;
+
+  guyPhelps = new GuyPhelps(canvas, context, 'Guy Phelps', 100, 770);
+  controller = new Controller(guyPhelps);
+
+  startPlank = new Plank("left", guyPhelps, context, 0, 820, 200);
+  submitPlank = new Plank("right", guyPhelps, context, canvas.width - 80, endAxis - 1, 80);
+  leapingPlank = new Plank("right", guyPhelps, context, canvas.width - 110, endAxis + 100, 130);
+
+  a = new LetterBlock(guyPhelps, 'a', 30, 20, context);
+  c = new LetterBlock(guyPhelps, 'c', 130, 550, context);
+  d = new LetterBlock(guyPhelps, 'd', 50, 420, context);
+  e = new LetterBlock(guyPhelps, 'e', 350, 20, context);
+  g = new LetterBlock(guyPhelps, 'g', 370, 570, context);
+  h = new LetterBlock(guyPhelps, 'h', 370, 290, context);
+  i = new LetterBlock(guyPhelps, 'i', 200, 20, context);
+  n = new LetterBlock(guyPhelps, 'n', 270, 440, context);
+  r = new LetterBlock(guyPhelps, 'r', 35, 280, context);
+  s = new LetterBlock(guyPhelps, 's', 120, 150, context);
+  t = new LetterBlock(guyPhelps, 't', 275, 170, context);
+  v = new LetterBlock(guyPhelps, 'v', 280, 700, context);
+
+  water = new Water(context, canvas);
+  allBlocks = [a, e, i, s, t, r, h, d, n, c, g, v];
+  allPlanks = [startPlank, submitPlank, leapingPlank];
+};
 
 const draw = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
@@ -60,7 +95,7 @@ const draw = () => {
   }
   if (water.y < endAxis || guyPhelps.y + guyPhelps.height >= water.y + 7) {
     alert("YOU LOSE!")
-    location.reload();
+    initialise();
   }
   if (guyPhelps.x === 520 && guyPhelps.y === 84) {
     submitScore(guyPhelps);
